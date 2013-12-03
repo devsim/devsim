5703199ab3b585569e7b3e9f75bcb91244c363c6
Juan <juanesanchez@github>
2013-12-02 20:32:53 -0600

    Use specific .cmake config file for each platform.

--- scripts/setup_osx_10.9.sh
+++ scripts/setup_osx_10.9.sh
@@ -1,9 +1,18 @@
 DEVSIM_CONFIG=osx_10.9
+CC=/usr/bin/gcc
+CXX=/usr/bin/g++
+CMAKE_CXX_FLAGS=""
 for TYPE in debug release; do
   for ARCH in x86_64; do
   #for ARCH in i386 x86_64; do
     NAME=osx_${ARCH}_${TYPE}
     mkdir ${NAME}
-    (cd $NAME; /usr/bin/cmake -DDEVSIM_CONFIG=${DEVSIM_CONFIG} -DCMAKE_OSX_ARCHITECTURES=${ARCH} -DCMAKE_BUILD_TYPE=${TYPE} -DCMAKE_CXX_COMPILER=/usr/bin/g++ -DCMAKE_C_COMPILER=/usr/bin/gcc ..)
+    (cd $NAME; cmake \
+      -DCMAKE_BUILD_TYPE=${TYPE} \
+      -DCMAKE_CXX_COMPILER=${CXX} \
+      -DCMAKE_C_COMPILER=${CC} \
+      -DDEVSIM_CONFIG=${DEVSIM_CONFIG} \
+      -DCMAKE_CXX_FLAGS="${CMAKE_CXX_FLAGS}" \
+      ..)
   done
 done
