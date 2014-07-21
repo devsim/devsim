037b91044a4a1523552b47967189aeeab0552b82
Juan E. Sanchez <juanesanchez@github>
2014-07-20 23:34:10 -0500

    Ubuntu 14.04 setup scripts

---
+++ scripts/setup_ubuntu_14.04.sh
@@ -0,0 +1,18 @@
+DEVSIM_CONFIG="ubuntu_14.04"
+CC=/usr/bin/gcc
+CXX=/usr/bin/g++
+CMAKE_CXX_FLAGS="-std=c++0x"
+for TYPE in debug release; do
+ARCH=`uname -m`
+#  for ARCH in i386 x86_64; do
+    NAME=linux_${ARCH}_${TYPE}
+    mkdir ${NAME}
+    (cd $NAME; cmake \
+      -DCMAKE_BUILD_TYPE=${TYPE} \
+      -DCMAKE_CXX_COMPILER=${CXX} \
+      -DCMAKE_C_COMPILER=${CC} \
+      -DDEVSIM_CONFIG=${DEVSIM_CONFIG} \
+      -DCMAKE_CXX_FLAGS="${CMAKE_CXX_FLAGS}" \
+      ..)
+#  done
+done
