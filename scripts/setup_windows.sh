f437d087187e20cfb66b124542ec243360ff4784
Juan E. Sanchez <juanesanchez@github>
2014-09-16 23:51:23 -0500

    windows 64 bit support

--- scripts/setup_windows.sh
+++ scripts/setup_windows.sh
@@ -1,8 +1,6 @@
 CMAKE=/cygdrive/C/Program\ Files\ \(x86\)/CMake/bin/cmake.exe
 DEVSIM_CONFIG=win32
-#mkdir -p win32/src/main/Release
-#mkdir -p win32/src/main/Debug
 mkdir -p win32
 (cd win32; "$CMAKE" -G "Visual Studio 12" -DDEVSIM_CONFIG=${DEVSIM_CONFIG} ..)
-#rsync -rv --exclude .svn lib win32/src/main/
-#cp -v ../../externallib_win32/AMD/acml4.4.0/ifort32/lib/*.dl4.4.0/ifort64/lib/*.dll win64/src/main/Debug
+mkdir -p win64
+(cd win64; "$CMAKE" -G "Visual Studio 12 Win64" -DDEVSIM_CONFIG=${DEVSIM_CONFIG} ..)
--- win32.cmake
+++ win32.cmake
@@ -7,8 +7,7 @@
 #SET (CMAKE_CXX_WARNING_LEVEL 4)
 SET (FLEX "C:/cygwin/bin/flex.exe")
 SET (BISON "C:/cygwin/bin/bison.exe")
-#SET (Boost_INCLUDE_DIRS "C:/Users/jsanchez/WindowsLibs/externallib_win32/boost_1_39_0") 
-#INCLUDE_DIRECTORIES(${Boost_INCLUDE_DIRS})
+
 ADD_DEFINITIONS(-DSTATIC_BUILD -D_USE_MATH_DEFINES -DTCL_THREADS)
 #Math stuff
 SET (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /fp:strict")
@@ -16,14 +15,14 @@ SET (CMAKE_C_FLAGS "${CMAKE_CXX_FLAGS} /fp:strict")
 SET (CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} /SAFESEH:NO")
 SET (CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} /SAFESEH:NO")
 SET (CMAKE_MODULE_LINKER_FLAGS "${CMAKE_MODULE_LINKER_FLAGS} /SAFESEH:NO")
-#ADD_DEFINITIONS(-DSTATIC_BUILD /fp:strict)
+
 #SET (CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} /NODEFAULTLIB:LIBCMT")
 #SET (CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} /NODEFAULTLIB:LIBCMT /NODEFAULTLIB:OLDNAMES")
-#
-#
+
 # windows build
 
-#IF (${CMAKE_SIZEOF_VOID_P} MATCHES 4)
+IF (${CMAKE_SIZEOF_VOID_P} MATCHES 4)
+# 32 bit
 SET (THIRDPARTY c:/build/devsim_external)
 SET (BLAS_ARCHIVE 
 ${THIRDPARTY}/CLAPACK/lib/lapack.lib
@@ -40,23 +39,38 @@ SET (SQLITE3_INCLUDE "${THIRDPARTY}/sqlite-amalgamation-3080600")
 SET (SUPERLULOCATE  ${THIRDPARTY}/SuperLU_4.3)
 SET (SUPERLU_ARCHIVE ${SUPERLULOCATE}/build/Release/superlu.lib)
 SET (SUPERLU_INCLUDE ${SUPERLULOCATE}/SRC)
-SET (SYMDIFF_INCLUDE c:/build/symdiff_github/include)
-SET (SYMDIFF_ARCHIVE c:/build/symdiff_github/lib/symdiff_static.lib)
+SET (SYMDIFF_INCLUDE c:/build/symdiff/include)
+SET (SYMDIFF_ARCHIVE c:/build/symdiff/win32/src/engine/Release/symdiff_static.lib)
 SET (TCL_ARCHIVE "c:/Tcl/lib/tcl85.lib")
 SET (TCL_INCLUDE "c:/Tcl/include")
 SET (TCL_STUB    "c:/Tcl/lib/tclstub85.lib")
 SET (ZLIB_ARCHIVE "${THIRDPARTY}/zlib/lib/zlibstatic.lib")
 SET (ZLIB_INCLUDE "${THIRDPARTY}/zlib/include")
 
-#ELSE (${CMAKE_SIZEOF_VOID_P} MATCHES 4)
-#SET(TCL_INCLUDE "c:/Tcl/include")
-#SET(TCL_STUB "c:/Tcl/lib/tclstub85.lib")
-#SET (SUPERLU_ARCHIVE ${SUPERLULOCATE}/lib/superlu_64.lib)
-#SET (BLAS_ARCHIVE  "${THIRDPARTY}/AMD/acml4.4.0/ifort64/lib/libacml_dll.lib")
-#SET (TCL_ARCHIVE "${THIRDPARTY}/tcl8.5.9/win/Release_AMD64_VC9/tcl85tsx.lib")
-#SET (ZLIB_ARCHIVE "${THIRDPARTY}/zlib-1.2.3/zlib_64.lib")
-#ENDIF (${CMAKE_SIZEOF_VOID_P} MATCHES 4)
+ELSE (${CMAKE_SIZEOF_VOID_P} MATCHES 4)
+# 64 bit
+SET (THIRDPARTY c:/build/devsim_external)
+SET (BLAS_ARCHIVE 
+${THIRDPARTY}/CLAPACK64/lib/lapack.lib
+${THIRDPARTY}/CLAPACK64/lib/blas.lib
+${THIRDPARTY}/CLAPACK64/lib/libf2c.lib
+)
 
-
-
+SET (CGNS_ARCHIVE ${THIRDPARTY}/cgnslib64/lib/cgns.lib)
+SET (CGNS_INCLUDE ${THIRDPARTY}/cgnslib64/include)
+SET (PYTHON_ARCHIVE C:/Python27_win64/libs/python27.lib)
+SET (PYTHON_INCLUDE C:/Python27_win64/include)
+SET (SQLITE3_ARCHIVE "${THIRDPARTY}/sqlite-amalgamation-3080600/build64/Release/sqlite3.lib")
+SET (SQLITE3_INCLUDE "${THIRDPARTY}/sqlite-amalgamation-3080600")
+SET (SUPERLULOCATE  ${THIRDPARTY}/SuperLU_4.3)
+SET (SUPERLU_ARCHIVE ${SUPERLULOCATE}/build64/Release/superlu.lib)
+SET (SUPERLU_INCLUDE ${SUPERLULOCATE}/SRC)
+SET (SYMDIFF_INCLUDE c:/build/symdiff/include)
+SET (SYMDIFF_ARCHIVE c:/build/symdiff/win64/src/engine/Release/symdiff_static.lib)
+SET (TCL_ARCHIVE "c:/Tcl_win64/lib/tcl85.lib")
+SET (TCL_INCLUDE "c:/Tcl_win64/include")
+SET (TCL_STUB    "c:/Tcl_win64/lib/tclstub85.lib")
+SET (ZLIB_ARCHIVE "${THIRDPARTY}/zlib64/lib/zlibstatic.lib")
+SET (ZLIB_INCLUDE "${THIRDPARTY}/zlib64/include")
+ENDIF (${CMAKE_SIZEOF_VOID_P} MATCHES 4)
 
