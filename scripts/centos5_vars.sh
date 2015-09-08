c64e90e422bce58cda31e69d884211a1c2fe0586
Juan E. Sanchez <juanesanchez@github>
2015-09-07 20:00:48 -0500

    Update centos5 with Intel MKL

---
+++ scripts/centos5_vars.sh
@@ -0,0 +1,7 @@
+###
+### Source this script on your platform to set proper
+### environment variables for using anaconda with devsim
+###
+export LD_LIBRARY_PATH=${HOME}/anaconda/lib
+export PYTHONHOME=${HOME}/anaconda
+export TCL_LIBRARY=${HOME}/anaconda/lib/tcl8.5
