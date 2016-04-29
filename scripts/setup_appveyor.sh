68ca6d150a9bb9fcabe5cd49fa543b256ff93e30
Juan E. Sanchez <juanesanchez@github>
2016-04-28 20:16:24 -0500

    enable Tcl build

--- scripts/setup_appveyor.sh
+++ scripts/setup_appveyor.sh
@@ -3,4 +3,4 @@ DEVSIM_CONFIG=appveyor
 #mkdir -p win32
 #(cd win32; "$CMAKE" -G "Visual Studio 14" -DDEVSIM_CONFIG=${DEVSIM_CONFIG} ..)
 mkdir -p win64
-(cd win64; "$CMAKE" -G "Visual Studio 14 Win64" -DDEVSIM_CONFIG=${DEVSIM_CONFIG} -DTCLMAIN=OFF ..)
+(cd win64; "$CMAKE" -G "Visual Studio 14 Win64" -DDEVSIM_CONFIG=${DEVSIM_CONFIG} -DTCLMAIN=ON ..)
