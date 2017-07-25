8f54bbdfb44c502ed650a37f62ed7c61224f4118
Juan E. Sanchez <juan@tcad.com>
2017-07-25 12:50:35 -0500

    fix appveyor cmake path

--- scripts/setup_appveyor.sh
+++ scripts/setup_appveyor.sh
@@ -1,5 +1,5 @@
 
-CMAKE=cmake
+CMAKE=/cygdrive/C/Program\ Files\ \(x86\)/CMake/bin/cmake.exe
 DEVSIM_CONFIG=appveyor
 #mkdir -p win32
 #(cd win32; "$CMAKE" -G "Visual Studio 14" -DDEVSIM_CONFIG=${DEVSIM_CONFIG} ..)
