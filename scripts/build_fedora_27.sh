9f0181e63a1e8ec475c5ad5fb4bc8b41fb121cf0
Juan E. Sanchez <juan@tcad.com>
2018-05-05 17:35:16 -0500

    fix fedora27 build dependency and do make -j4

--- scripts/build_fedora_27.sh
+++ scripts/build_fedora_27.sh
@@ -14,7 +14,8 @@ if [[ ${EUID} -ne 0 ]]; then
 fi
 ${SUDO} dnf install -y git bison flex tcl tcl-devel cmake3 gcc gcc-c++ libquadmath-devel \
      gcc-gfortran bzip2 boost boost-devel cgnslib cgnslib-devel \
-     sqlite-devel python2-devel python3-devel blas-devel lapack-devel
+     sqlite-devel python2-devel python3-devel blas-devel lapack-devel \
+     make
 
 mkdir -p ${DSDIR}
 
@@ -46,7 +47,7 @@ cd ${DSDIR}/devsim/external/getrf &&
 mkdir -p build &&
 cd build &&
 cmake -DCMAKE_Fortran_FLAGS="-freal-8-real-16 -ffixed-line-length-0" -DCMAKE_BUILD_TYPE=RELEASE .. &&
-make -j2
+make -j4
 )
 
 
@@ -55,14 +56,14 @@ make -j2
 (
 cd ${DSDIR}/devsim/external/symdiff &&
 ./scripts/setup_fedora_27.sh &&
-cd linux_x86_64_release && make -j2
+cd linux_x86_64_release && make -j4
 )
 
 # Finally, build devsim
 (
 cd ${DSDIR}/devsim &&
 scripts/setup_fedora_27.sh &&
-cd linux_x86_64_release && make -j2
+cd linux_x86_64_release && make -j4
 )
 #(cd dist && bash package_linux.sh ${1})
 
