0165e2ad9675b354e86cc47dc99feb09452665c6
Juan E. Sanchez <juan@tcad.com>
2018-10-22 17:55:26 -0500

    remove numpy as requirement

--- scripts/build_centos_6.sh
+++ scripts/build_centos_6.sh
@@ -20,7 +20,7 @@ then
 curl -O https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh;
 bash ~/Miniconda2-latest-Linux-x86_64.sh -b -p ${HOME}/anaconda;
 fi
-${HOME}/anaconda/bin/conda install -y numpy mkl mkl-devel mkl-include cmake
+${HOME}/anaconda/bin/conda install -y mkl mkl-devel mkl-include cmake
 ${HOME}/anaconda/bin/conda create -y --name python36 python=3.6
 #${HOME}/anaconda/bin/conda install -y -n python36 numpy mkl-devel mkl-include
 ${HOME}/anaconda/bin/conda create -y --name python37 python=3.7
--- scripts/build_osx.sh
+++ scripts/build_osx.sh
@@ -45,7 +45,7 @@ then
 (cd ${HOME} &&
 curl -O https://repo.continuum.io/miniconda/Miniconda2-latest-MacOSX-x86_64.sh;
 bash ~/Miniconda2-latest-MacOSX-x86_64.sh -b -p ${HOME}/anaconda;
-${HOME}/anaconda/bin/conda install -y numpy mkl mkl-devel mkl-include;)
+${HOME}/anaconda/bin/conda install -y mkl mkl-devel mkl-include;)
 
 #Python3
 ${HOME}/anaconda/bin/conda create -y --name python36 python=3.6
