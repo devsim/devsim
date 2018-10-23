f040f2583b3e5ecd7f3c146ff07088a81b891b3f
Juan E. Sanchez <juan@tcad.com>
2018-10-22 19:58:20 -0500

    work on fixing build with minimal python installs

--- scripts/build_centos_6.sh
+++ scripts/build_centos_6.sh
@@ -20,6 +20,8 @@ then
 curl -O https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh;
 bash ~/Miniconda2-latest-Linux-x86_64.sh -b -p ${HOME}/anaconda;
 # Python 2
+# install cmake in base environment
+${HOME}/anaconda/bin/conda install cmake
 ${HOME}/anaconda/bin/conda create  -y --name python27_devsim_build python=2.7
 ${HOME}/anaconda/bin/conda install -y --name python27_devsim_build mkl mkl-devel mkl-include
 #Python3
