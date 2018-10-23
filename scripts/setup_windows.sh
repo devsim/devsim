0f5a01f04f6180639ca8125179fcf46e67da1a2b
Juan E. Sanchez <juan@tcad.com>
2018-10-22 23:05:59 -0500

    remove obsolete scripts

--- scripts/setup_windows.sh
+++
@@ -1,6 +0,0 @@
-CMAKE=/cygdrive/C/Program\ Files\ \(x86\)/CMake/bin/cmake.exe
-DEVSIM_CONFIG=win32
-mkdir -p win32
-(cd win32; "$CMAKE" -G "Visual Studio 14" -DDEVSIM_CONFIG=${DEVSIM_CONFIG} ..)
-mkdir -p win64
-(cd win64; "$CMAKE" -G "Visual Studio 14 Win64" -DDEVSIM_CONFIG=${DEVSIM_CONFIG} ..)
