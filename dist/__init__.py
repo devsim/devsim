c7ae246fce12748f9e6a3e7584e2ca045a1d9138
Juan E. Sanchez <juan@tcad.com>
2018-10-26 20:00:13 -0500

    add version variable

--- dist/__init__.py
+++ dist/__init__.py
@@ -6,12 +6,16 @@ import sys
 
 if sys.version_info[0] == 2:
   from devsim_py27 import *
+  from devsim_py27 import __version__
 elif sys.version_info[0] == 3 and sys.version_info[1] == 6:
   from .devsim_py36 import *
+  from .devsim_py36 import __version__
 elif sys.version_info[0] == 3 and sys.version_info[1] == 7:
   from .devsim_py37 import *
+  from .devsim_py37 import __version__
 else:
   raise ImportError('module not available for Python %d.%d please contact technical support' % sys.version_info[0:2])
+
 
 
 
