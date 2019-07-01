d242d510e3c427807c514adfaad20a9f058dc1b4
Juan E. Sanchez <juan@tcad.com>
2019-07-01 10:37:44 -0500

    add tanh and make more functions float128

--- src/MathEval/MathEval.cc
+++ src/MathEval/MathEval.cc
@@ -88,6 +88,7 @@ namespace Eqomfp {
   {"acosh",     acosh,        "acosh(obj)   -- inverse hyperbolic cosine function"},
   {"asinh",     asinh,        "asinh(obj)   -- inverse hyperbolic sine function"},
   {"atanh",     atanh,        "atanh(obj)   -- inverse hyperbolic tangent function"},
+  {"tanh",      tanh,         "tanh(obj)   -- hyperbolic tangent function"},
   {"erf",       erf,          "erf(obj)   -- error function"},
   {"erfc",      erfc,         "erfc(obj)  -- complementary error function"},
   {"derfdx",    derfdx,       "derfdx(obj)   -- derivative error function"},
@@ -165,41 +166,37 @@ float128 dInvFermidx(float128 x)
 
 float128 acosh(float128 x)
 {
-  return std::acosh(static_cast<double>(x));
+  return boost::multiprecision::acosh(x);
 }
 
 float128 asinh(float128 x)
 {
-  return std::asinh(static_cast<double>(x));
+  return boost::multiprecision::asinh(x);
 }
 
 float128 atanh(float128 x)
 {
-  return std::atanh(static_cast<double>(x));
+  return boost::multiprecision::atanh(x);
 }
 
+float128 tanh(float128 x)
+{
+  return boost::multiprecision::tanh(x);
+}
 
 float128 erf(float128 x)
 {
-  return ::erf(static_cast<double>(x));
+  return boost::multiprecision::erf(x);
 }
 
 float128 erfc(float128 x)
 {
-  return ::erfc(static_cast<double>(x));
+  return boost::multiprecision::erfc(x);
 }
 
 
-float128 derfdx(float128 x)
-{
-  return ::derfdx(static_cast<double>(x));
-}
-
-
-float128 derfcdx(float128 x)
-{
-  return ::derfcdx(static_cast<double>(x));
-}
+using ::derfdx;
+using ::derfcdx;
 }
 
 
@@ -215,6 +212,7 @@ float128 derfcdx(float128 x)
   {"acosh",    eval128::acosh,        "acosh(obj)   -- inverse hyperbolic cosine function"},
   {"asinh",    eval128::asinh,        "asinh(obj)   -- inverse hyperbolic sine function"},
   {"atanh",    eval128::atanh,        "atanh(obj)   -- inverse hyperbolic tangent function"},
+  {"tanh",     eval128::tanh,         "tanh(obj)   -- hyperbolic tangent function"},
   {"erf",      eval128::erf,          "erf(obj)   -- error function"},
   {"erfc",     eval128::erfc,         "erfc(obj)  -- complementary error function"},
   {"derfdx",   eval128::derfdx,       "derfdx(obj)   -- derivative error function"},
