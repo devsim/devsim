c0714593cc34d9ce1c4cbc10f962daaaebd1045e
Juan E. Sanchez <juan@tcad.com>
2019-12-13 18:06:30 -0600

    add appveyor file

---
+++ scripts/appveyor_msys.bat
@@ -0,0 +1,4 @@
+
+@setlocal
+::set PATH="C:\msys64\mingw64\bin;C:\msys64\usr\bin;%PATH%"
+c:\msys64\usr\bin\bash scripts\build_msys.sh %*
--- scripts/build_msys.sh
+++ scripts/build_msys.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 set -e
-
+export PATH=/mingw64/bin:/usr/bin:${PATH}
 # msys Specific
 
 # handle miniconda in appveyor.yml
 
 # this script assumes git clone and submodule initialization has been done
 
+# SYMDIFF build
+(cd external/symdiff && bash ../symdiff_msys.sh && cd msys_x86_64_release && make -j2);
+
 # SuperLU
 (cd external && bsdtar xzf superlu_4.3.tar.gz && bash build_superlu_msys.sh)
 
-# SYMDIFF build
-(cd external/symdiff && bash ../symdiff_msys.sh && cd msys_x86_64_release && make -j2);
 
 # quad precision getrf
 (cd external/getrf && bash setup_msys.sh && cd build && make -j2)
@@ -20,5 +21,4 @@ set -e
 bash scripts/setup_msys.sh
 (cd msys_x86_64_release && make -j2)
 (cd dist && bash package_msys.sh ${1})
-
 
