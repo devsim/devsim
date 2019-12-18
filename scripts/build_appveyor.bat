ea48cb04819e20262fbe19ac226b8482ee058e6a
Juan E. Sanchez <juan@tcad.com>
2019-12-17 20:52:11 -0600

    try msys

--- scripts/build_appveyor.bat
+++ scripts/build_appveyor.bat
@@ -1,5 +1,6 @@
 
 :: SET_USE_CYGWIN is true for Visual Studio Builds
+IF "%PLATFORM%"=="msys" SET CONDA_PATH=c:\Miniconda37-x64\Scripts\conda && SET BUILDDIR=msys
 IF "%PLATFORM%"=="x64" SET CONDA_PATH=c:\Miniconda37-x64\Scripts\conda && SET BUILDDIR=win64 && SET USE_CYGWIN=true
 IF "%PLATFORM%"=="x86" SET CONDA_PATH=c:\Miniconda37\Scripts\conda && SET BUILDDIR=win32 && SET USE_CYGWIN=true
 
@@ -13,10 +14,8 @@ IF DEFINED USE_CYGWIN (
 
   setup-x86.exe -qnNdO -R C:/cygwin -s http://cygwin.mirror.constant.com -l C:/cygwin/var/cache/setup -P flex -P bison -P zip
   if %errorlevel% neq 0 exit /b %errorlevel%
-)
 
 :: BUILD DEPENDENCIES
-IF DEFINED USE_CYGWIN (
   cd %APPVEYOR_BUILD_FOLDER%\external
   c:\cygwin\bin\bash.exe ./build_superlu_appveyor.sh %PLATFORM%
   if %errorlevel% neq 0 exit /b %errorlevel%
@@ -28,10 +27,8 @@ IF DEFINED USE_CYGWIN (
   cd %BUILDDIR%
   cmake --build . --config Release -- /m /nologo /verbosity:minimal
   if %errorlevel% neq 0 exit /b %errorlevel%
-)
 
 :: BUILD DEVSIM
-IF DEFINED USE_CYGWIN (
   cd %APPVEYOR_BUILD_FOLDER%
   c:\cygwin\bin\bash.exe scripts/setup_appveyor.sh %PLATFORM%
   if %errorlevel% neq 0 exit /b %errorlevel%
@@ -39,12 +36,15 @@ IF DEFINED USE_CYGWIN (
   cd %BUILDDIR%
   cmake --build . --config Release -- /m /v:m
   if %errorlevel% neq 0 exit /b %errorlevel%
-)
 
 :: PACKAGE DEVSIM
-IF DEFINED USE_CYGWIN (
   cd %APPVEYOR_BUILD_FOLDER%\dist
   c:\cygwin\bin\bash.exe package_appveyor.sh devsim_%BUILDDIR%_%APPVEYOR_REPO_TAG_NAME%
+  if %errorlevel% neq 0 exit /b %errorlevel%
+) ELSE (
+  %CONDA_PATH% install --yes mkl mkl-devel mkl-include sqlite zlib boost
+  cd %APPVEYOR_BUILD_FOLDER%
+  c:\msys64\usr\bin\bash %APPVEYOR_BUILD_FOLDER%\scripts\build_msys.sh devsim_%BUILDDIR%_%APPVEYOR_REPO_TAG_NAME%
   if %errorlevel% neq 0 exit /b %errorlevel%
 )
 
