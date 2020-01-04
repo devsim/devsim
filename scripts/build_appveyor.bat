da25a9d241ea0dcd59658c3fd4fe93fd45ac607a
Juan E. Sanchez <juan@tcad.com>
2020-01-04 11:57:40 -0600

    fix minor issue

--- scripts/build_appveyor.bat
+++ scripts/build_appveyor.bat
@@ -17,19 +17,19 @@ if %errorlevel% neq 0 exit /b %errorlevel%
 call %CONDA_BIN% activate devsim_build
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-:: SET_USE_VISUALSTUDIO is true for Visual Studio Builds
 IF "%1"=="msys" (
   SET BUILDDIR=msys
   SET PACKAGE_NAME="devsim_msys_%TAG%"
-  SET BASH_SCRIPT=%BASEDIR%\scripts\build_msys.sh
+  SET USE_MSYS=true
   call %CONDA_BIN% install -y -n devsim_build boost
+  SET BASH_SCRIPT=%BASEDIR%\scripts\build_msys.sh
 )
 
 IF "%1"=="x64" (
   SET GENERATOR="Visual Studio 16 2019"
   SET AOPTION="x64"
   SET BUILDDIR=win64
-  SET USE_VISUALSTUDIO=true
+  SET USE_VISUAL_STUDIO=true
   SET PACKAGE_NAME="devsim_win64_%TAG%"
   SET BASH_SCRIPT=%BASEDIR%\scripts\build_appveyor.sh
 )
@@ -38,12 +38,15 @@ IF "%1"=="x86" (
   SET GENERATOR="Visual Studio 16 2019"
   SET AOPTION="Win32"
   SET BUILDDIR=win32
-  SET USE_VISUALSTUDIO=true
+  SET USE_VISUAL_STUDIO=true
   SET PACKAGE_NAME="devsim_win32_%TAG%"
   SET BASH_SCRIPT=%BASEDIR%\scripts\build_appveyor.sh
 )
 
 cd %BASEDIR%
-c:\msys64\usr\bin\bash %BASH_SCRIPT% %GENERATOR% %AOPTION% %BUILDDIR% %CONDA_PREFIX% %PACKAGE_NAME%
+IF DEFINED USE_VISUAL_STUDIO c:\msys64\usr\bin\bash %BASH_SCRIPT% %GENERATOR% %AOPTION% %BUILDDIR% %CONDA_PREFIX% %PACKAGE_NAME%
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+IF DEFINED USE_MSYS c:\msys64\usr\bin\bash %BASH_SCRIPT% %PACKAGE_NAME%
 if %errorlevel% neq 0 exit /b %errorlevel%
 
