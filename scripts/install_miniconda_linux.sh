a7b209ccdbafc09fea462250787ce76277e6bd39
Juan E. Sanchez <juan@tcad.com>
2020-09-25 22:46:42 -0500

    build devsim ubuntu

---
+++ scripts/install_miniconda_linux.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+cd ${HOME}
+if [ ! -f Miniconda3-latest-Linux-x86_64.sh ]
+then
+curl -L -O https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh;
+bash Miniconda3-latest-Linux-x86_64.sh -b -p ${HOME}/anaconda;
+fi
+${HOME}/anaconda/bin/conda create  -y --name python37_devsim_build python=3.7
+${HOME}/anaconda/bin/conda install -y --name python37_devsim_build mkl mkl-devel mkl-include boost cmake
+source ${HOME}/anaconda/bin/activate python37_devsim_build
+
---
+++ scripts/install_ubuntu_depend.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -e
+
+apt install -y flex bison curl gcc gfortran g++ make
+
+#TODO: CLEAN apt
