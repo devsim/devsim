a9cb1329ad0a48f69c08f49926f0817ee9ca43ba
Juan E. Sanchez <juan@tcad.com>
2020-10-18 23:25:56 -0500

    add git

--- scripts/install_ubuntu_depend.sh
+++ scripts/install_ubuntu_depend.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-apt install -y flex bison curl gcc gfortran g++ make cmake libsqlite3-dev zlib1g-dev python3-dev python3 liblapack3 libblas3
+apt install -y flex bison curl gcc gfortran g++ make cmake libsqlite3-dev zlib1g-dev python3-dev python3 liblapack3 libblas3 git
 #TODO: CLEAN apt
