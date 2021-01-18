9a4b7f552edaea69ed5b1e9a72fa404903d8af07
Juan E. Sanchez <juan@tcad.com>
2021-01-18 11:33:14 -0600

    update docs

--- src/pythonapi/DevsimDoc.cc
+++ src/pythonapi/DevsimDoc.cc
@@ -115,7 +115,7 @@ R"(    devsim.set_circuit_node_value (solution, node, value)
 )";
 
 static const char contact_equation_doc[] =
-R"(    devsim.contact_equation (device, contact, name, variable_name, circuit_node, edge_charge_model, edge_current_model, edge_model, edge_volume_model, element_charge_model, element_current_model, element_model, volume_model, node_charge_model, node_current_model, node_model)
+R"(    devsim.contact_equation (device, contact, name, variable_name, circuit_node, edge_charge_model, edge_current_model, edge_model, edge_volume_model, element_charge_model, element_current_model, element_model, volume_node0_model, volume_node1_model, node_charge_model, node_current_model, node_model)
 
     Create a contact equation on a device
 
@@ -145,8 +145,10 @@ R"(    devsim.contact_equation (device, contact, name, variable_name, circuit_no
        Name of the element edge model used to determine the current flowing out of this contact
     element_model : str, optional
        Name of the element edge model being integrated at each edge at this contact
-    volume_model : str, optional
-       Name of the element model being integrated over the volume of each edge on the contact
+    volume_node0_model : str, optional
+       Name of the element model being integrated over the volume of node 0 of each edge on the contact
+    volume_node1_model : str, optional
+       Name of the element model being integrated over the volume of node 1 of each edge on the contact
     node_charge_model : str, optional
        Name of the node model used to determine the charge at this contact
     node_current_model : str, optional
@@ -214,7 +216,7 @@ R"(    devsim.delete_interface_equation (device, interface, name)
 )";
 
 static const char equation_doc[] =
-R"(    devsim.equation (device, region, name, variable_name, node_model, edge_model, edge_volume_model, time_node_model, element_model, volume_model, variable_update)
+R"(    devsim.equation (device, region, name, variable_name, node_model, edge_model, edge_volume_model, time_node_model, element_model, volume_node0_model, volume_node1_model, variable_update)
 
     Specify an equation to solve on a device
 
@@ -238,8 +240,10 @@ R"(    devsim.equation (device, region, name, variable_name, node_model, edge_mo
        Name of the time dependent node_model being integrated at each node in the device volume
     element_model : str, optional
        Name of the element model being integrated over each edge in the device volume
-    volume_model : str, optional
-       Name of the element model being integrated over the volume of each edge in the device volume
+    volume_node0_model : str, optional
+       Name of the element model being integrated over the volume of node 0 of each edge on the contact
+    volume_node1_model : str, optional
+       Name of the element model being integrated over the volume of node 1 of each edge on the contact
     variable_update : {'default', 'log_damp', 'positive'}
        update type for circuit variable
 
