16ac934f71083cc7fb4b62453fa2c8b896e38d08
Juan E. Sanchez <juan@tcad.com>
2021-02-06 00:36:41 -0600

    try and fix automated build

--- scripts/build_centos_7.sh
+++ scripts/build_centos_7.sh
@@ -19,7 +19,7 @@ if [ ! -f Miniconda3-latest-Linux-x86_64.sh ]
 then
 curl -L -O https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh;
 bash Miniconda3-latest-Linux-x86_64.sh -b -p ${HOME}/anaconda;
-${HOME}/anaconda/bin/conda create  -y --name python3_devsim_build python=3
+${HOME}/anaconda/bin/conda create  -y --name python3_devsim_build python=3.8
 ${HOME}/anaconda/bin/conda install -y --name python3_devsim_build mkl mkl-devel mkl-include boost cmake
 fi
 source ${HOME}/anaconda/bin/activate python3_devsim_build
