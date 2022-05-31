2d9e01c6af0e76e915b1aeaf2b42464fd496a6a0
Juan E. Sanchez <juan@tcad.com>
2022-05-30 21:22:36 -0500

    update build scripts

--- scripts/build_centos_7.sh
+++ scripts/build_centos_7.sh
@@ -23,28 +23,16 @@ export PYTHON3_BIN=python
 export PYTHON3_INCLUDE=$(python -c "from sysconfig import get_paths as gp; print(gp()['include'])")
 export PYTHON3_ARCHIVE=""
 
-#download boost library
-#(cd ${HOME}/devsim/external && curl -O -L https://dl.bintray.com/boostorg/release/1.66.0/source/boost_1_66_0.tar.gz)
-#(cd ${HOME}/devsim/external && tar xzf boost_1_66_0.tar.gz)
-
-# SuperLU
-#(cd external && curl -O http://crd-legacy.lbl.gov/~xiaoye/SuperLU/superlu_4.3.tar.gz && tar xzf superlu_4.3.tar.gz)
-(cd external && tar xzf superlu_4.3.tar.gz)
-
 # SYMDIFF build
 (cd external/symdiff && bash ../symdiff_centos.sh && cd linux_x86_64_release && make -j2);
 
-# CGNSLIB build
-####(cd external && curl -L -O https://github.com/CGNS/CGNS/archive/v3.1.4.tar.gz && tar xzf v3.1.4.tar.gz)
-####(cd external && mkdir -p CGNS-3.1.4/build && cd CGNS-3.1.4/build && cmake3  -DCMAKE_C_COMPILER=/opt/rh/devtoolset-8/root/usr/bin/gcc -DBUILD_CGNSTOOLS=OFF -DCMAKE_INSTALL_PREFIX=$PWD/../../cgnslib .. && make -j2 && make install)
 # SUPERLU build
-(cd external/SuperLU_4.3 && sh ../superlu_centos6.sh)
+(cd external/superlu && sh ../superlu_centos6.sh)
 
 # quad precision getrf
 (cd external/getrf && bash setup_centos6.sh && cd build && make -j2)
 
 # start devsim build
-
 bash scripts/setup_centos_6.sh
 
 # overcome bad libstdc++ in anaconda directory
--- scripts/build_centos_8.sh
+++ scripts/build_centos_8.sh
@@ -13,7 +13,6 @@ export CC="/usr/bin/gcc"
 export CXX="/usr/bin/g++"
 export F77="/usr/bin/gfortran"
 
-cd ${HOME}
 #minimal conda environments to prevent linking against the wrong libraries
 # now opt for explicit dll load of mkl
 #conda create  -y --name python3_devsim_build python=3 mkl mkl-devel mkl-include boost cmake
@@ -24,24 +23,9 @@ export PYTHON3_BIN=python
 export PYTHON3_INCLUDE=$(python -c "from sysconfig import get_paths as gp; print(gp()['include'])")
 export PYTHON3_ARCHIVE=""
 
-#download boost library
-#(cd ${HOME}/devsim/external && curl -O -L https://dl.bintray.com/boostorg/release/1.66.0/source/boost_1_66_0.tar.gz)
-#(cd ${HOME}/devsim/external && tar xzf boost_1_66_0.tar.gz)
-
-
-# this script assumes git clone and submodule initialization has been done
-cd devsim
-
-# SuperLU
-#(cd external && curl -O http://crd-legacy.lbl.gov/~xiaoye/SuperLU/superlu_4.3.tar.gz && tar xzf superlu_4.3.tar.gz)
-#(cd external && tar xzf superlu_4.3.tar.gz)
-
 # SYMDIFF build
 (cd external/symdiff && bash ../symdiff_centos.sh && cd linux_x86_64_release && make -j2);
 
-# CGNSLIB build
-####(cd external && curl -L -O https://github.com/CGNS/CGNS/archive/v3.1.4.tar.gz && tar xzf v3.1.4.tar.gz)
-####(cd external && mkdir -p CGNS-3.1.4/build && cd CGNS-3.1.4/build && cmake3  -DCMAKE_C_COMPILER=/opt/rh/devtoolset-8/root/usr/bin/gcc -DBUILD_CGNSTOOLS=OFF -DCMAKE_INSTALL_PREFIX=$PWD/../../cgnslib .. && make -j2 && make install)
 # SUPERLU build
 (cd external/superlu && sh ../superlu_centos6.sh)
 
@@ -49,7 +33,6 @@ cd devsim
 (cd external/getrf && bash setup_centos6.sh && cd build && make -j2)
 
 # start devsim build
-
 bash scripts/setup_centos_6.sh
 
 # overcome bad libstdc++ in anaconda directory
