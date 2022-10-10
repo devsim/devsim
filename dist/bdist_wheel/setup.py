94a2c02263495be077e748bf5ca068466e016db6
Juan E. Sanchez <juan@tcad.com>
2022-10-10 02:22:16 -0500

    work on setup.py

--- dist/bdist_wheel/setup.py
+++ dist/bdist_wheel/setup.py
@@ -6,6 +6,10 @@ module1 = Extension('devsim.foo',
                     py_limited_api = True,
                     sources = ['foo.c'])
 setup(name='devsim',
+      url = 'https://devsim.org',
+      author = "Juan Sanchez and others",
+      author_email = "info@devsim.com",
+      description = "DEVSIM: TCAD Device Simulator",
       version = '1.0.0',
       #include_package_data = True,
       packages=['devsim', 'devsim.python_packages'],
@@ -13,13 +17,21 @@ setup(name='devsim',
         '' : 'lib',
       },
       package_data={
-        'devsim' : ['./*.so', '../testing/*.py',],
+        'devsim' : ['./*.so', './*.dylib', './*.pyd',],
       },
       ext_modules = [module1],
       data_files = [
         ('', glob('*.*')),
         ('doc', glob('doc/devsim.pdf')),
-        ('testing', glob('testing/*')),
+        ('testing', glob('testing/*.*')),
         ('examples', glob('examples/**/*.*', recursive=True)),
-      ]
+      ],
+         classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3",
+        "Topic :: Scientific/Engineering :: Electronic Design Automation (EDA)",
+
+    ],
     )
