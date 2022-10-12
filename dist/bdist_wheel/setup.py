e8e7782f74eb47db5736cab2d1c68864ed6ec25c
Juan E. Sanchez <juan@tcad.com>
2022-10-12 15:21:27 -0500

    update work on wheel

--- dist/bdist_wheel/setup.py
+++ dist/bdist_wheel/setup.py
@@ -1,37 +1,26 @@
-#setup.py
-from setuptools import setup, Extension
+from setuptools import setup, Distribution
 from glob import glob
 
-module1 = Extension('devsim.foo',
-                    py_limited_api = True,
-                    sources = ['foo.c'])
-setup(name='devsim',
-      url = 'https://devsim.org',
-      author = "Juan Sanchez and others",
-      author_email = "info@devsim.com",
-      description = "DEVSIM: TCAD Device Simulator",
-      version = '1.0.0',
-      #include_package_data = True,
+#https://stackoverflow.com/questions/35112511/pip-setup-py-bdist-wheel-no-longer-builds-forced-non-pure-wheels/36886459#36886459
+#https://stackoverflow.com/questions/24071491/how-can-i-make-a-python-wheel-from-an-existing-native-library/24793171#24793171
+class BinaryDistribution(Distribution):
+    """Distribution which always forces a binary package with platform name"""
+    def has_ext_modules(foo):
+        return True
+
+setup(
       packages=['devsim', 'devsim.python_packages'],
       package_dir = {
         '' : 'lib',
       },
       package_data={
-        'devsim' : ['./*.so', './*.dylib', './*.pyd',],
+        'devsim' : ['./*.so', './*.dll', './*.dylib', './*.pyd',],
       },
-      ext_modules = [module1],
+      distclass=BinaryDistribution,
       data_files = [
-        ('', glob('*.*')),
-        ('doc', glob('doc/devsim.pdf')),
-        ('testing', glob('testing/*.*')),
-        ('examples', glob('examples/**/*.*', recursive=True)),
+        ('devsim_data', glob('*.*')),
+        ('devsim_data/doc', glob('doc/devsim.pdf')),
+        ('devsim_data/testing', glob('testing/*.*')),
+        ('devsim_data/examples', glob('examples/**/*.*', recursive=True)),
       ],
-         classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Intended Audience :: Science/Research",
-        "License :: OSI Approved :: Apache Software License",
-        "Programming Language :: Python :: 3",
-        "Topic :: Scientific/Engineering :: Electronic Design Automation (EDA)",
-
-    ],
     )
