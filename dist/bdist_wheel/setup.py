3a54bf04e7518806d29d63e64dfd56f91c7697a7
Juan E. Sanchez <juan@tcad.com>
2022-10-22 16:02:07 -0500

    Issue #89: Fix up markdown url in the pypi readme

--- dist/bdist_wheel/setup.py
+++ dist/bdist_wheel/setup.py
@@ -2,6 +2,7 @@ from setuptools import setup, Distribution
 from glob import glob
 from itertools import chain
 import os
+import re
 
 #https://stackoverflow.com/questions/35112511/pip-setup-py-bdist-wheel-no-longer-builds-forced-non-pure-wheels/36886459#36886459
 #https://stackoverflow.com/questions/24071491/how-can-i-make-a-python-wheel-from-an-existing-native-library/24793171#24793171
@@ -23,6 +24,21 @@ def generate_data_files():
 data_files = [('devsim_data', glob('*.*'))]
 data_files.extend(generate_data_files())
 
+
+def fix_readme(text):
+    regex = re.compile('\[([^]]+)\]+\((?!http[s]:\/\/)([^)]+)\)')
+    base_url = os.environ.get('README_BASE_URL')
+    if base_url:
+        if base_url[-1] != '/':
+            base_url += '/'
+        print("Fixing up base url for long description {0}".format(base_url))
+        text = regex.sub(lambda m, urlbase=base_url : r'[{0}]({1})'.format(m.group(1), urlbase + m.group(2)), text)
+    return text
+
+with open('README.md', 'r') as ifile:
+    text = ifile.read()
+    text = fix_readme(text)
+
 setup(
       packages=['devsim', 'devsim.python_packages'],
       package_dir = {
@@ -32,5 +48,7 @@ setup(
         'devsim' : ['./*.so', './*.dll', './*.dylib', './*.pyd',],
       },
       distclass=BinaryDistribution,
-      data_files = data_files
+      data_files = data_files,
+      long_description = text,
+      long_description_content_type="text/markdown",
     )
--- scripts/build_docker_manylinux2014.sh
+++ scripts/build_docker_manylinux2014.sh
@@ -4,7 +4,8 @@ if [[ $# -eq 0 ]]; then
     echo 'no tag name!'
     exit -1
 fi
-docker run -it -d --name manylinux2014 quay.io/pypa/manylinux2014_x86_64 &&
+
+docker run -it -d -e README_BASE_URL="${README_BASE_URL}" --name manylinux2014 quay.io/pypa/manylinux2014_x86_64 &&
 (cd .. && tar czf devsim.tgz devsim && docker cp devsim.tgz manylinux2014:/root/) &&
 docker exec manylinux2014 bash -c "git config --global --add safe.directory /root/devsim"
 docker exec manylinux2014 bash -c "cd /root && tar xzf devsim.tgz";
@@ -13,3 +14,4 @@ docker exec manylinux2014 bash -c "cd /root/devsim && bash scripts/build_manylin
 (cd dist && docker cp manylinux2014:/root/devsim/dist/devsim_linux_${1}.tgz .);
 (cd dist && for i in $(docker exec manylinux2014 bash -c "ls /root/devsim/dist/*.whl"); do docker cp manylinux2014:${i} .; done)
 docker stop manylinux2014
+
