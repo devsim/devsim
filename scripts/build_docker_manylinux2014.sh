33185e950ae541c858b2993a9196e897dafca411
Juan E. Sanchez <juan@tcad.com>
2024-01-03 12:37:06 -0600

    fix typo

--- scripts/build_docker_manylinux2014.sh
+++ scripts/build_docker_manylinux2014.sh
@@ -12,7 +12,7 @@ docker run -it -d -e README_BASE_URL="${README_BASE_URL}" --name manylinux2014 q
 (cd .. && tar czf devsim.tgz devsim && docker cp devsim.tgz manylinux2014:/root/) &&
 docker exec manylinux2014 bash -c "git config --global --add safe.directory /root/devsim"
 docker exec manylinux2014 bash -c "cd /root && tar xzf devsim.tgz";
-docker exec manylinux2014 bash -c "cd /root/devsim && bash scripts/build_manylinux2014.sh ${DEVSIM_TAR_NAME}}";
+docker exec manylinux2014 bash -c "cd /root/devsim && bash scripts/build_manylinux2014.sh ${DEVSIM_TAR_NAME}";
 (cd dist && docker cp manylinux2014:/root/devsim/dist/${DEVSIM_TAR_NAME}.tgz .);
 (cd dist && for i in $(docker exec manylinux2014 bash -c "ls /root/devsim/dist/*.whl"); do docker cp manylinux2014:${i} .; done)
 docker stop manylinux2014
