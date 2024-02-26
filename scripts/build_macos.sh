f46b942f724e0ea9adbf4252712b0a54ac8ff84c
Juan E. Sanchez <juan@tcad.com>
2024-02-26 13:18:51 -0600

    work on universal build

--- scripts/build_macos.sh
+++ scripts/build_macos.sh
@@ -27,8 +27,8 @@ elif [ "${1}" = "clang" ]
   then
   export CMAKE="cmake"
   export CMAKE_CXX_FLAGS=""
-  export CC=/usr/bin/gcc;
-  export CXX=/usr/bin/g++;
+  export CC=clang;
+  export CXX=clang++;
   export F77="";
   export ARCH_ARG="-DCMAKE_OSX_ARCHITECTURES=arm64;x86_64"
   export PLAT_NAME="universal"
@@ -81,6 +81,8 @@ DIST_NAME=devsim_macos_${PLAT_NAME}_${2}
 cp -f dist/bdist_wheel/setup.* dist/${DIST_NAME}
 FULL_PLAT_NAME=$(${PYTHON3_BIN} dist/bdist_wheel/fix_macos_arch.py ${PLAT_NAME})
 echo PACKAGING $FULL_PLAT_NAME
-(cd dist/${DIST_NAME} &&  perl -p -i -e "s/^#plat-name.*/plat-name = ${FULL_PLAT_NAME}/" setup.cfg)
+if [[ -n "$FULL_PLAT_NAME" ]]; then
+(cd dist/${DIST_NAME} &&  perl -p -i -e "s/^#plat-name.*/plat-name = ${FULL_PLAT_NAME}/" setup.cfg);
+fi
 (cd dist/${DIST_NAME} && ${PIP_BIN} wheel .)
 (cp dist/${DIST_NAME}/*.whl dist)
